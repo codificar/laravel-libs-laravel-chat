da7a2462a6724b9fd273ce0aa4af8f6ef7e77e22
João Aires <airesjoao25@outlook.com>
2020-09-24 20:01:47 -0300

    Initial commit

---
+++ src/Http/controllers/RideChatController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Codificar\LaravelChat\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class RideChatController extends Controller
+{
+
+}
---
+++ src/LaravelChatServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Codificar\LaravelChat;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelChatServiceProvider extends ServiceProvider 
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
