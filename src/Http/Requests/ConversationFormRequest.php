60e5f3855975f393bacebf9a00eaf9ea7eb3f768
randler <randler.ferraz@codificar.com.br>
2022-09-14 18:34:57 -0300

    criando uma conversation caso não exista

--- src/Http/Requests/ConversationFormRequest.php
+++ src/Http/Requests/ConversationFormRequest.php
@@ -43,6 +43,45 @@ class ConversationFormRequest extends FormRequest
 			$sender_type = $this->userType;
 		}
 
+		// verifica se tem uma conversation para a request 
+		$convId = null;
+		if($this->request_id) {
+			$convRequest = \ConversationRequest::where(['request_id' => $this->request_id])->first();
+			if(isset($convRequest->conversation_id) && !empty($convRequest->conversation_id)) {
+				$convId = $convRequest->conversation_id;
+			}
+		}
+		
+		//verifica se tem uma conversation criada, caso não, 
+		// cria uma para se inscrever no socket da conversação de forma correta
+		$conversation = \Conversation::find($convId);
+		if ($this->request_id && !$conversation) {
+			try {
+				$request = \Requests::find($this->request_id);
+				$userOne = \Ledger::where(['user_id' => $request->user_id])->first()->id;
+				$userTwo = \Ledger::where(['provider_id' => $request->current_provider])->first()->id;
+
+				$conversation = new \Conversation();
+				$conversation->user_one = $userOne;
+				$conversation->user_two = $userTwo;
+				$conversation->request_id = $request->id;
+				$conversation->help_id = null;
+				$conversation->status = 1;
+				$conversation->save();
+
+				$convRequest = new \ConversationRequest();
+				$convRequest->conversation_id = $conversation->id;
+				$convRequest->request_id = $request->id;
+				$convRequest->provider_accepted = isset($request->current_provider) && !empty($request->current_provider) 
+					? 1
+					: 0;
+				$convRequest->user_accepted = 0;
+				$convRequest->save();
+			} catch (\Exception $e) {
+				\Log::error($e->getMessage());
+			}
+		}
+
 		if($sender_type == "provider") {
 			$ledger_id = $this->provider->ledger->id;
 		} else if($sender_type == "admin") {
